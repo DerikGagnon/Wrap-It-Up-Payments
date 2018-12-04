ebb00ecd953d360c1408fe23c379878aee68b319
DerikGagnon <derik.gagnon@mymail.champlain.edu>
2018-12-04 09:19:40 -0500

    Working Prototype

--- FoodTruckPOS/Item/ItemCell.swift
+++ FoodTruckPOS/Item/ItemCell.swift
@@ -30,10 +30,10 @@ class ItemCell: UITableViewCell {
         // Initialization code
     }
     
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-        
-        // Configure the view for the selected state
-    }
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
     
 }
