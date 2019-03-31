3cd64e93ebe1ce8467fe0874c5dfeb8d5a11f48c
DerikGagnon <derik.gagnon@mymail.champlain.edu>
2019-03-31 13:03:27 -0400

    User Login -> Place order

--- WrapItUpPayments/ViewControllers/UserLoginViewController.swift
+++ WrapItUpPayments/ViewControllers/UserLoginViewController.swift
@@ -15,8 +15,31 @@ class UserLoginViewController: UIViewController, FUIAuthDelegate {
     fileprivate(set) var authUI: FUIAuth? //only set internally but get externally
     fileprivate(set) var authStateListenerHandle: AuthStateDidChangeListenerHandle?
     
+    func authUI(_ authUI: FUIAuth, didSignInWith user: User?, error: Error?) {
+        guard let authError = error else {
+            print("Login Successful")
+            return
+        }
+        
+        let errorCode = UInt((authError as NSError).code)
+        
+        switch errorCode {
+        case FUIAuthErrorCode.userCancelledSignIn.rawValue:
+            print("User cancelled sign-in");
+            break
+            
+        default:
+            let detailedError = (authError as NSError).userInfo[NSUnderlyingErrorKey] ?? authError
+            print("Login error: \((detailedError as! NSError).localizedDescription)");
+        }
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        print(Auth.auth().currentUser?.displayName ?? "name")
+        try! Auth.auth().signOut()
 
         // Do any additional setup after loading the view.
         // Do any additional setup after loading the view, typically from a nib.
@@ -35,9 +58,19 @@ class UserLoginViewController: UIViewController, FUIAuthDelegate {
         
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        //print(Auth.auth().currentUser?.displayName ?? "name")
+        if Auth.auth().currentUser != nil {
+            print("Inside If")
+            print(Thread.isMainThread)
+            self.performSegue(withIdentifier: "LoginSegue", sender: self)
+        }
+    }
+    
     @IBAction func loginAction(sender: AnyObject) {
         // Present the default login view controller provided by authUI
         let authViewController = authUI?.authViewController();
+        print("authenticator")
         self.present(authViewController!, animated: true, completion: nil)
         
     }
